a997940a5c21c3b55a1f5daf9d3d8133ae5c9917
BarrozoL <barrozo.lucasp@gmail.com>
2024-10-26 11:21:00 +0100

    render.com server

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const app = require("./app");
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
 
-//Changed port to 3000 and added "0.0.0.0" to match flyctl configs toml file
+/* to use fly.io use: const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});  */
